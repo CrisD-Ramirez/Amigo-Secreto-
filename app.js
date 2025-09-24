a97797aba0ea1ab45e50c312f84bc545661ba460
Christopher Ramirez <cd.ramirez@uned.cr>
2025-09-23 20:07:54 -0600

    Se agregaron las funciones pedidas

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Array para guardar los nombres de los amigos 
+let amigos = [] 
+console.log(amigos);
+
+function agregarAmigo() {
+        let inputAmigo = document.getElementById('amigo');
+    let nombre = inputAmigo.value;
+
+    if (nombre === '') {
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+        
+    amigos.push(nombre);
+    inputAmigo.value = '';
+
+    // Actualizar  lista 
+    actualizarListaAmigos();
+    console.log(amigos);
+}
+
+function actualizarListaAmigos() {
+  // Obtener el elemento de la lista
+  const lista = document.getElementById("listaAmigos");
+
+  // Limpiar la lista existente
+  lista.innerHTML = "";
+
+  // Iterar sobre el arreglo
+  for (let i = 0; i < amigos.length; i++) {
+    // Agregar elementos a la lista
+    const li = document.createElement("li");
+    li.textContent = amigos[i];
+    lista.appendChild(li);
+  }
+}
+
+function sortearAmigo() {
+  // Comprobar que la lista no este vacia 
+  if (amigos.length === 0) {
+    alert("No hay amigos disponibles para sortear.");
+  } else {
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    const amigoSorteado = amigos[indiceAleatorio];
+  
+    const elementoResultado = document.getElementById('resultado');
+        elementoResultado.innerHTML = amigoSorteado;
+  }
+}
